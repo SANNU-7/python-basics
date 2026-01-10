a085bfc21941a7433622195c689f31003609935e
Sanan Shah <sanan726266@gmail.com>
2026-01-10 20:34:12 +0500

    Add files via upload

---
+++ 02_Variables and datatype/01_variables and datatype.py
@@ -0,0 +1,19 @@
+"""
+Identifier → the name
+
+Variable → the container
+
+Data type → the type of value inside the container
+
+"""
+
+# For example age = 20
+
+"""
+age = 20 it is also variable because it store the 20 integer
+
+age → identifier
+
+20 → data type (int)
+
+"""
