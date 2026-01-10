87b8f2dafc2198d5934705ad72da3bafdb6fb5c8
Sanan Shah <sanan726266@gmail.com>
2026-01-10 20:35:26 +0500

    Add files via upload

---
+++ 05_input function/input.py
@@ -0,0 +1,16 @@
+#  input function takes input from the user 
+
+# s = input("enter first no : ")
+# w = input("enter second no : ")
+# print(" first no is : " , s)
+# print(" second no is : " , w)
+# print(" sum is : ", s + w) 
+# # this program will concatinate the value of s and w like sanan + shah = sananshah 
+
+# to add or subtract or for some thing else 
+
+a = int(input("enter first no : "))
+b = int(input("enter second no : "))
+print(" first no is : " , a)
+print(" second no is : " , b)
+print(" sum is : ", a + b) # this will sum the a and b value 
