934bad040354a1384d44312714c86b58b0ddb5af
Sanan Shah <sanan726266@gmail.com>
2026-01-12 10:18:23 +0500

    Add files via upload

---
+++ 02_Variables and datatype/02_Variables and datatype/01_variables and datatype.py
@@ -0,0 +1,35 @@
+
+# 🔢 int – whole numbers
+age = 20
+print(type(age))  # this will print the data type integer
+
+
+# 🔢 float – decimal numbers
+price = 99.5
+print(type(price)) # this will print the data type float
+
+
+# 📝 str – text
+language = "Python"
+print(type(language)) # this will print the data type string
+
+
+# ✅ bool – True or False
+is_learning = True
+print(type(is_learning)) # this will print the data type bool
+
+
+
+"""
+Identifier → the name
+Variable → the container
+Data type → the type of value inside the container
+"""
+
+# For example age = 20
+"""
+age = 20 it is also variable because it store the 20 integer
+age → identifier
+20 → data type (int)
+"""
+
