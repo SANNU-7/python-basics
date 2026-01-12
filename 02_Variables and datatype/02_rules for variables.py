c3ab0417ed315b8072c493910757a185484a5dc4
Sanan Shah <sanan726266@gmail.com>
2026-01-12 10:20:14 +0500

    Add files via upload

---
+++ 02_Variables and datatype/02_rules for variables.py
@@ -0,0 +1,14 @@
+# Rules for Variables in Python
+
+"""
+• A variable name can contain letters, digits, and underscores (_).
+• A variable name must start with a letter or an underscore.
+• A variable name cannot start with a digit.
+• No whitespace is allowed in variable names.
+
+Examples of valid variable names:
+name
+one8
+seven
+_seven
+"""
